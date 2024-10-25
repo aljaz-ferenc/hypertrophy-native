6d778c7119ae4f4c426bd23ca293f20de476c160
Aljaž Ferenc <aljaz.ferenc@specto.si>
2024-10-25 14:53:11 +0200

    style, translations

--- components/molecules/WeeklyNutritionChart.tsx
+++ components/molecules/WeeklyNutritionChart.tsx
@@ -3,13 +3,33 @@ import { styleConstants } from "@/constants/styles";
 import { Center, View } from "native-base";
 import { Dimensions } from "react-native";
 import { BarChart } from "react-native-gifted-charts/dist/BarChart";
+import {useMemo} from "react";
+import {useTranslation} from "react-i18next";
 
 type WeightChartProps = {
   weight: { value: number }[];
 };
 
 export default function WeightChart({ weight }: WeightChartProps) {
-  console.log("WEIGHT: ", weight);
+    const {t} = useTranslation()
+
+    const weightData = useMemo(() => {
+        const arr = new Array(7 - weight.length).fill({value: 0})
+        return [...weight, ...arr]
+    }, [weight])
+
+    const days = useMemo(() => {
+        return [
+            t("NUTRITION.daysAbbr.mon"),
+            t("NUTRITION.daysAbbr.tue"),
+            t("NUTRITION.daysAbbr.wed"),
+            t("NUTRITION.daysAbbr.thu"),
+            t("NUTRITION.daysAbbr.fri"),
+            t("NUTRITION.daysAbbr.sat"),
+            t("NUTRITION.daysAbbr.sun"),
+        ]
+    }, [t])
+
   return (
     <View style={{ alignItems: "center", justifyContent: "center" }}>
       <Center>
@@ -20,8 +40,11 @@ export default function WeightChart({ weight }: WeightChartProps) {
         frontColor={Colors.white}
         yAxisThickness={0}
         xAxisThickness={0}
+        maxValue={2500}
+        xAxisLabelTexts={days}
+        xAxisLabelTextStyle={{color: 'white'}}
         yAxisTextStyle={{color: Colors.white}}
-          data={weight}
+          data={weightData}
         />
       </Center>
     </View>
